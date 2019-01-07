1736aa6c41450780d82a03a6fe85d5f3692cdee8
Matthew Wire (MJW Consulting) <mjw@mjwconsult.co.uk>
2019-01-07 14:02:43 +0000

    Add hook to override qrcode data / token values

---
+++ CRM/Qrcodecheckin/Hook.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class CRM_Qrcodecheckin_Hook
+ *
+ * This class implements hooks for Qrcodecheckin
+ */
+class CRM_Qrcodecheckin_Hook {
+
+  /**
+   * This hook allows to alter qrcodecheckin tokens.
+   *
+   * @param array $values Array of token values for current contactId
+   * @param int $contactId
+   * @param bool $handled - Set to TRUE if your hook handled the token values, FALSE to allow default handling
+   *
+   * @return mixed
+   */
+  public static function tokenValues(&$values, $contactId, &$handled) {
+    return CRM_Utils_Hook::singleton()
+      ->invoke(3, $values, $contactId, $handled, CRM_Utils_Hook::$_nullObject,
+        CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, 'civicrm_qrcodecheckin_tokenValues');
+  }
+
+}
