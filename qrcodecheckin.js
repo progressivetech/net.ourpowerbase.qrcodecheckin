185b35b187b8f3c38b4093fd61ad7901cc636131
Jamie McClelland <jm@mayfirst.org>
2019-02-06 10:53:55 -0500

    enable wordpress to extract participant_id.

--- qrcodecheckin.js
+++ qrcodecheckin.js
@@ -1,8 +1,38 @@
 CRM.$('#qrcheckin-update-button').click(function() {
-  // Get participant_id from the URL 
-  // (/civicrm/qrcodecheckin/123/blahblahhash)
-  var reg = /\/qrcodecheckin\/([0-9]+)\//
-  var participant_id = reg.exec(window.location.pathname)[1];
+  var participant_id;
+
+  // Try Drupal first.
+  // We expect: /civicrm/qrcodecheckin/123/blahblahhash
+  // We want: 123
+  var reg_drupal = /\/qrcodecheckin\/([0-9]+)\//
+  var match = reg_drupal.exec(window.location.pathname);
+  if (match) {
+    participant_id = match[1];
+  }
+  else {
+    // Try wordpress
+    // We expect: /wp-admin/admin.php?page=CiviCRM&q=civicrm%2Fqrcodecheckin%2F65%2Fa21855da08cb102d1d217c53dc5824a3a795c1c1a44e971bf01ab9da3a2acbbf
+    // We want: 65
+    //var reg_wordpress = /\/wp-admin\/admin\.php/
+    var reg_wordpress_path = /test\.html/
+    var reg_wordpress_extract_q = /[?&]q=([^&]+)(&|$)/ 
+    var reg_wordpress_extract_id = /^civicrm\/qrcodecheckin\/([0-9]+)\//
+    var path_match = reg_wordpress_path.exec(window.location.pathname);
+    if (path_match) {
+      // Now we have to extract the query string q.
+      var q_match = reg_wordpress_extract_q.exec(location.search);
+      if (q_match) {
+        // Now URL decode it.
+        q = decodeURIComponent(q_match[1].replace(/\+/g, " "));
+        // Now we have: civicrm/qrcodecheckin/65/a21855da08cb102d1d217c53dc5824a3a795c1c1a44e971bf01ab9da3a2acbbf
+        participant_match = reg_wordpress_extract_id.exec(q);
+        if (participant_match) {
+          participant_id = participant_match[1];
+        }
+      }
+    }
+  }
+ 
   CRM.api3('Qrcodecheckin', 'Checkin', {
     "sequential": 1,
     "participant_id": participant_id
